77c55081737abe267b648382e1112a09c05546ee
Jason Cumberland <jason.cumberland@gmail.com>
2025-11-02 11:02:31 -0700

    Update roomie-activity-importer.groovy

--- apps/roomie-activity-importer.groovy
+++ apps/roomie-activity-importer.groovy
@@ -5,8 +5,8 @@ definition(
   description: "Bulk import Roomie activities as Alexa-controllable virtual switches.",
   category: "Convenience",
   singleInstance: true,
-  iconUrl: "https://raw.githubusercontent.com/myrison/hubitat-roomie-integration/main/icons/roomie_logo2_x.png",
-  iconX2Url: "https://raw.githubusercontent.com/myrison/hubitat-roomie-integration/main/icons/roomie_logo.png",
+  iconUrl: "https://raw.githubusercontent.com/myrison/hubitat-roomie-integration/main/icons/roomie_logo.png",
+  iconX2Url: "https://raw.githubusercontent.com/myrison/hubitat-roomie-integration/main/icons/roomie_logo_2x.png",
 )
 
 private String h2(String txt) {
